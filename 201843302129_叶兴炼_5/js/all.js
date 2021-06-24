c5bc26718909644101aaf6879d8577ec2008fa94
yexiaolian <13536751477@163.com>
2021-06-24 17:08:06 +0800

    new

---
+++ 201843302129_叶兴炼_5/js/all.js
@@ -0,0 +1,63 @@
+// $(document).ready(function () {
+    // $('#removeModal').on('show.bs.modal', function (event) {
+    //     var button = $(event.relatedTarget)
+    //     var title = button.data('title')
+    //     var modal = $(this)
+    //     modal.find('.modal-title').text('確認' + title)
+    // })
+    
+// });
+
+$(function () {
+    $('#removeModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget)
+        var title = button.data('title')
+        var modal = $(this)
+        modal.find('.modal-title').text('確認' + title)
+    })
+    
+    // 加号点击事件
+    $(".add").click(function () {
+        var v = Number($(this).parent().find("input:eq(1)").attr("value")) + 1;
+        // 更新数量
+        $(this).parent().find("input:eq(1)").attr("value", v);
+        
+        // 更新总价
+        var ele = $(this).parent().parent();
+        var totalprice = Number(ele.find("td:eq(1)").find("p").text()) * Number($(this).parent().find("input:eq(1)").attr("value"));
+        ele.find("td:eq(3)").find("p").text(totalprice + ".00");
+    })
+    // 减号点击事件
+    $(".sub").click(function () {
+        var v = Number($(this).parent().find("input:eq(1)").attr("value"));
+        var ele = $(this).parent().parent();
+        var name = ele.find("td:first").text();
+        if (v - 1 == 0) {
+            if (confirm("是否删除商品【" + name + "】")) {
+                ele.remove();
+            }
+        } else {
+            $(this).parent().find("input:eq(1)").attr("value", v - 1);
+            var totalprice = Number(ele.find("td:eq(1)").find("p").text()) * Number($(this).parent().find("input:eq(1)").attr("value"));
+            ele.find("td:eq(3)").find("p").text(totalprice + ".00");
+        }
+    })
+})
+
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
